bccf2c726c45ee31a64e62289f26919da3fa9bd0
Soyhan <soyhan.parlar@students.fhnw.ch>
2016-10-31 14:33:12 +0100

    PraxisProjekt
    DataModel

---
+++ src/Surgery.java
@@ -0,0 +1,65 @@
+import java.sql.Time;
+import java.util.Date;
+import java.util.IdentityHashMap;
+
+/**
+ * Created by TheGod on 31.10.16.
+ */
+public class Surgery {
+    private Date opDate;
+    private String opRoom;
+    private Time start;
+    private Time end;
+    private Integer opDuaration;
+    private String meetingIdentifier;
+    private String comment1;
+    private String comment2;
+    private String comment3;
+    private String meetingNotice;
+    private Integer tattCode;
+    private Integer taSequence;
+    private Integer taCode;
+    private Integer knotenID;
+    private Integer taStatistic;
+    private Integer knotenIdentifier;
+    private Integer taIdnetifier;
+    private String operatuer;
+    private String assistance;
+    private String anaesthesia;
+    private String anaesthesiaCare;
+    private String instrumentation;
+    private String zudienung;
+    private String patientFirstName;
+    private String patientLastName;
+    private Date birthDate;
+    private char gender;
+    private Integer age;
+    private String room;
+    private Integer pID;
+    private Integer fID;
+    private String stay;
+    private String discipline;
+    private String oe;
+    private String Klasse;
+    private String categorie;
+    private Date entrance;
+    private Time entranceTime;
+    private Date leaving;
+    private Time leavingTime;
+    private Time p1PatientRequest;
+    private Time p2PatientArrive;
+    private Time p3StartWrangle;
+    private Time p4EndWrangle;
+    private Time p5PatientInOp;
+    private Time p6EnrolmentToCareUnit;
+    private Time p7PatientOutOfOp;
+
+
+
+
+
+
+
+
+
+}
