090a78c4d17524b228259bcbaf7c89422bc98a13
Soyhan <soyhan.parlar@students.fhnw.ch>
2016-11-14 13:16:42 +0100

    Info Label für mergeDatei

--- src/App.java
+++ src/App.java
@@ -19,7 +19,18 @@ import java.util.Iterator;
 import java.util.Set;
 
 public class App extends Application {
+    File firstFile;
+    File secondFile;
 
+    ArrayList<Surgery> surgeries;
+    ArrayList<Surgery> surgeries2;
+
+
+
+
+
+
+  //  File mergedFile = new File("/Users/TheGod/Desktop/Praxisprojekt/src/newFile.csv");
 
     public static void main(String[] args) {
         launch(args);
@@ -27,6 +38,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
 
         final GridPane gridPane = new GridPane();
         gridPane.setPadding(new Insets(10, 10, 10, 10));
@@ -39,10 +51,10 @@ public class App extends Application {
         Label label = new Label("Wähle die gewünschte Datei:");
         Button button = new Button("Datei Auswahl!");
         FileChooser fileChooser = new FileChooser();
-        chooseFile(fileChooser);
+        fileChooserExtensionFilter(fileChooser);
 
         // Label zeigt FileName an
-        Label getFileNameLabel = new Label();
+
         // Info Label
         Label ausgewählteDateiLabel = new Label("Die ausgewählte Datei:");
         Label ausgewählteDateiLabel2 = new Label("Die ausgewählte Datei:");
@@ -51,10 +63,10 @@ public class App extends Application {
         Label label2 = new Label("Wähle die gewünschte Datei:");
         Button button2 = new Button("Datei Auswahl!");
         FileChooser fileChooser2 = new FileChooser();
-        chooseFile(fileChooser2);
+        fileChooserExtensionFilter(fileChooser2);
 
         // Label zeigt FileName an
-        Label getFileNameLabel2 = new Label();
+
 
 // Label & Button für Mergen
         Label mergeLabel = new Label("Mergen:");
@@ -63,66 +75,73 @@ public class App extends Application {
 
 // ButtonAktivität für die erste Datei
         button.setOnAction(e -> {
+            Label getNameFirstFile;
             // FileChooser für die erste Datei
-            File file = fileChooser.showOpenDialog(primaryStage);
+          firstFile = fileChooser.showOpenDialog(null);
+            getNameFirstFile = new Label(firstFile.getName());
+
+
             Parser parser = null;
             try {
-                parser = Parser.resolveParser(file);
-
-                String fileName = file.getName();
-                getFileNameLabel.setText(fileName);
-
+                parser = Parser.resolveParser(firstFile);
             } catch (Exception e1) {
                 e1.printStackTrace();
             }
             try {
-                // List surgeries wird erstellt
-                ArrayList<Surgery> surgeries = parser.parse(file);
-
-//------------------------------------------------------------------------------------------------------------------
-                button2.setOnAction(e1 -> {
-
-                    // 2. FileChooser für die zweite Datei
-                    File file2 = fileChooser2.showOpenDialog(primaryStage);
-                    Parser parser2 = null;
-                    try {
-                        parser2 = Parser.resolveParser(file2);
-
-                        String fileName2 = file2.getName();
-                        getFileNameLabel2.setText(fileName2);
-
-                    } catch (Exception e2) {
-                        e2.printStackTrace();
-                    }
-
-                    try {
-                        // Liste surgeries2 wird erstellt
-                        ArrayList<Surgery> surgeries2 = parser2.parse(file2);
-
-                        // surgeries & surgeries2 zu newList zusammenführen
-                        Set<Surgery> newSet = new HashSet<Surgery>(surgeries);
-                        newSet.addAll(surgeries2);
-                        ArrayList<Surgery> newList = new ArrayList<Surgery>(newSet);
-
-//------------------------------------------------------------------------------------------------------------------
-
-                        // Button zum Mergen
-                        mergeButton.setOnAction(e3 -> {
-                            // newList in eine CSV Datei einlesen
-                            listInDatei(newList, new File("/Users/TheGod/Desktop/Praxisprojekt/src/newFile.csv"));
-                            info.setText("Merge Datei wurde erstellt!");
-
-                        });
-                    } catch (Exception e2) {
-                        e2.printStackTrace();
-                    }
-                });
-
+                surgeries = parser.parse(firstFile);
             } catch (Exception e1) {
                 e1.printStackTrace();
             }
         });
 
+
+
+//------------------------------------------------------------------------------------------------------------------
+
+        button2.setOnAction(e -> {
+
+            // 2. FileChooser für die zweite Datei
+            secondFile = fileChooser2.showOpenDialog(primaryStage);
+
+
+
+            Parser parser2 = null;
+            try {
+                parser2 = Parser.resolveParser(secondFile);
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+            try {
+                 surgeries2 = parser2.parse(secondFile);
+
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        });
+/*
+        Label getName1;
+        Label getName2 = new Label();*/
+
+       /* getName1 = new Label(firstFile.getName());
+        getName2 = new Label(firstFile.getName());*/
+
+
+
+
+//------------------------------------------------------------------------------------------------------------------
+        // Button zum Mergen
+        mergeButton.setOnAction(e -> {
+
+// surgeries & surgeries2 zu newList zusammenführen
+            Set<Surgery> newSet = new HashSet<Surgery>(surgeries);
+            newSet.addAll(surgeries2);
+            ArrayList<Surgery> newList = new ArrayList<Surgery>(newSet);
+
+
+            // newList in eine CSV Datei einlesen
+            listInDatei(newList, new File("/Users/TheGod/Desktop/Praxisprojekt/src/newFile.csv"));
+            info.setText("Merge Datei wurde erstellt!");
+        });
 
 
 // Zuordnung zum Grid (Node, X-Position, Y-Position)
@@ -131,9 +150,9 @@ public class App extends Application {
         gridPane.add(label2, 0, 1);
         gridPane.add(button2, 1, 1);
         gridPane.add(ausgewählteDateiLabel, 0, 2);
-        gridPane.add(getFileNameLabel, 1, 2);
+       // gridPane.add(getName1, 1, 2);
         gridPane.add(ausgewählteDateiLabel2, 0, 3);
-        gridPane.add(getFileNameLabel2, 1, 3);
+       // gridPane.add(getName2, 1, 3);
         gridPane.add(mergeLabel, 0, 4);
         gridPane.add(mergeButton, 1, 4);
         gridPane.add(info, 0, 5);
@@ -145,7 +164,6 @@ public class App extends Application {
     }
 
 
-
     // liest eine Liste in eine CSV Datei ein und erstellt eine CSV Datei
     private static void listInDatei(ArrayList<Surgery> list, File file) {
         PrintWriter printWriter = null;
@@ -153,17 +171,15 @@ public class App extends Application {
             printWriter = new PrintWriter(new FileWriter(file));
             Iterator iter = list.iterator();
 
-            printWriter.println("opDate" +";"+"opRoom"+";"+"start" +";"
-                    +";"+"end"+";"+"opDuaration"+";"+"meetingIdentifier"+";"+"comment1"+";"+"comment2"+";"+"comment3"+";"+"meetingNotice"+";"+
-                    "tattCode"+";"+"taCode"+";"+"taStatistic"+";"+
-                    "taIdnetifier"+";"+"operatuer"+";"+"assistance"+";"+"anaesthesia"+";"+"anaesthesiaCare"+";"+"instrumentation"+";"+"zudienung"+";"+"patientLastName"+";"+
-                    "patientFirstName"+";"+"birthDate"+";"+"gender"+";"+"age"+";"+"room"+";"+"pID"+";"+"fID"
-                    +";"+"stay"+";"+"discipline"+";"+"oe"+";"+"klasse"+";"+";"+"entranceTime"+";"+"leaving"+";"+"leavingTime"+";"+"p1PatientRequest"+";"+"p3StartWrangle"+";"+
-                    "p5PatientInOp"+";"+"o5StartClean"+";"+"o6StartPraesenzFirstOperateur"+";"+"o8StartOp"+";"+"o10EndOp"+";"+"o11End"+";"+"p7PatientOutOfOp"+";"+
-                    "notFallSpezifikation"+";"+"terminAbweichung"+";"+"terminAbweichungVorbereitung"+";"+
-                    "terminAbweichungWiederaufbereitung"+";"+"terminArtID"+";"+"terminArtCode"+";"+"terminID");
-
-
+            printWriter.println("opDate" + ";" + "opRoom" + ";" + "start" + ";"
+                    + ";" + "end" + ";" + "opDuaration" + ";" + "meetingIdentifier" + ";" + "comment1" + ";" + "comment2" + ";" + "comment3" + ";" + "meetingNotice" + ";" +
+                    "tattCode" + ";" + "taCode" + ";" + "taStatistic" + ";" +
+                    "taIdnetifier" + ";" + "operatuer" + ";" + "assistance" + ";" + "anaesthesia" + ";" + "anaesthesiaCare" + ";" + "instrumentation" + ";" + "zudienung" + ";" + "patientLastName" + ";" +
+                    "patientFirstName" + ";" + "birthDate" + ";" + "gender" + ";" + "age" + ";" + "room" + ";" + "pID" + ";" + "fID"
+                    + ";" + "stay" + ";" + "discipline" + ";" + "oe" + ";" + "klasse" + ";" + ";" + "entranceTime" + ";" + "leaving" + ";" + "leavingTime" + ";" + "p1PatientRequest" + ";" + "p3StartWrangle" + ";" +
+                    "p5PatientInOp" + ";" + "o5StartClean" + ";" + "o6StartPraesenzFirstOperateur" + ";" + "o8StartOp" + ";" + "o10EndOp" + ";" + "o11End" + ";" + "p7PatientOutOfOp" + ";" +
+                    "notFallSpezifikation" + ";" + "terminAbweichung" + ";" + "terminAbweichungVorbereitung" + ";" +
+                    "terminAbweichungWiederaufbereitung" + ";" + "terminArtID" + ";" + "terminArtCode" + ";" + "terminID");
 
 
             while (iter.hasNext()) {
@@ -181,10 +197,16 @@ public class App extends Application {
     }
 
     // FileChooser ExtensionFilter
-    public void chooseFile(FileChooser fileChooser) {
+    public void fileChooserExtensionFilter(FileChooser fileChooser) {
         FileChooser.ExtensionFilter extFilter =
                 new FileChooser.ExtensionFilter("CSV files", "*.csv");
         fileChooser.getExtensionFilters().add(extFilter);
+    }
+
+
+    public static Label getNameFile(File file){
+        Label getName = new Label(file.getName());
+        return getName;
     }
 
 
